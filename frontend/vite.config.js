7cfff7bce892388a3f28de7c2c00fbff9cb3c558
yvanhojunior <jyvan711@gmail.com>
2025-12-03 11:55:20 +0100

    pousse du fichier render.yaml

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+                secure: false,
+                rewrite: function (path) { return path.replace(/^\/api/, ''); }, // Remove /api prefix when proxying
+            },
+        },
+    },
+});
